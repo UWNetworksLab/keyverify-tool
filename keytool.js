859c207fc89947c0b4f2db2179889e324d8d47e2
Lally Singh <lally@google.com>
2016-01-29 16:14:26 -0500

    Key import works.

--- keytool.js
+++ keytool.js
@@ -46,75 +46,69 @@ var argv = require('yargs')
 var filename = argv._[0];
 var loaded_messages = {}
 
-var raw_key_password = 'password';
+var raw_key_password = '';
 var raw_keys = [
   {
-    "private": "-----BEGIN PGP PRIVATE KEY BLOCK-----\n" +
-        "Version: GnuPG v2\n" +
+    "private":
+        "-----BEGIN PGP PRIVATE KEY BLOCK-----\n" +
+        "Charset: UTF-8\n" +
+        "Version: End-To-End v0.31337.1\n" +
         "\n" +
-        "lKUEVqKVaRMIKoZIzj0DAQcCAwQj1vXJdzlfHhiXV5s0d9HK5q6edPgG9+uQVqYX\n" +
-        "/aJy/BWbSDKfX8IHmKRhfB6Msg8XFxq3UbaeVuZNtgEifURX/gcDArDnmWfqHUtt\n" +
-        "4jWQBri84gK6IMzvE/8TX902ntKCKqj/i2SgP85dzxFiAaaiWgupiQKJ33CygK4d\n" +
-        "sf2rRnv771jceuVLSv1G8Jzny13Zx/+0G3VzZXItMCA8dXNlci0wQGV4YW1wbGUu\n" +
-        "Y29tPoh5BBMTCAAhBQJWopVpAhsDBQsJCAcCBhUICQoLAgQWAgMBAh4BAheAAAoJ\n" +
-        "EPcjztauAk2VBnQA/1fZHalPbVxALq09Yq7FNzghakHgoyEkwekbQ+KNGAeCAPsG\n" +
-        "Hsd2asFC70XAnYADKJGxtKebNmHfWVyA6AjGJyFB75ypBFailWkSCCqGSM49AwEH\n" +
-        "AgMEQqgnUFOXnwI6OMJnaIVtXl4c3OgdInRgUfjpT//PFhYz2FPqt4c/1pHYhzx/\n" +
-        "6QqggObexKdY+dRZFS0DgL44MQMBCAf+BwMCB773+rJ/ncviTheSXe6VTgboxrTd\n" +
-        "gKiQhCOAcByz/2cUUdyeeJ1p2qyzwNK1+8oWWiuGpbIS+FCpwTZO/ChqiVQed4i5\n" +
-        "eD6IITRDuhaO5YhhBBgTCAAJBQJWopVpAhsMAAoJEPcjztauAk2VJQAA/18vW/TV\n" +
-        "ZJTTD8JuOvtzNWConrcH/BzSUhgHTgLgr5DQAQD33WqSY/Jodb0hvu9+rl1kK35u\n" +
-        "qqr9UVQ3ueoCthcknA==\n" +
-        "=UuAm\n" +
-        "-----END PGP PRIVATE KEY BLOCK-----",
-    "public": "-----BEGIN PGP PUBLIC KEY BLOCK-----\n" +
-        "Version: GnuPG v2\n" +
-        "\n" +
-        "mFIEVqKVaRMIKoZIzj0DAQcCAwQj1vXJdzlfHhiXV5s0d9HK5q6edPgG9+uQVqYX\n" +
-        "/aJy/BWbSDKfX8IHmKRhfB6Msg8XFxq3UbaeVuZNtgEifURXtBt1c2VyLTAgPHVz\n" +
-        "ZXItMEBleGFtcGxlLmNvbT6IeQQTEwgAIQUCVqKVaQIbAwULCQgHAgYVCAkKCwIE\n" +
-        "FgIDAQIeAQIXgAAKCRD3I87WrgJNlQZ0AP9X2R2pT21cQC6tPWKuxTc4IWpB4KMh\n" +
-        "JMHpG0PijRgHggD7Bh7HdmrBQu9FwJ2AAyiRsbSnmzZh31lcgOgIxichQe+4VgRW\n" +
-        "opVpEggqhkjOPQMBBwIDBEKoJ1BTl58COjjCZ2iFbV5eHNzoHSJ0YFH46U//zxYW\n" +
-        "M9hT6reHP9aR2Ic8f+kKoIDm3sSnWPnUWRUtA4C+ODEDAQgHiGEEGBMIAAkFAlai\n" +
-        "lWkCGwwACgkQ9yPO1q4CTZUlAAD/Xy9b9NVklNMPwm46+3M1YKietwf8HNJSGAdO\n" +
-        "AuCvkNABAPfdapJj8mh1vSG+736uXWQrfm6qqv1RVDe56gK2FySc\n" +
-        "=vXCi\n" +
-        "-----END PGP PUBLIC KEY BLOCK-----"
-  },
+        "xf8AAAB3BAAAAAATCCqGSM49AwEHAgMESZrm3yXlt+KgUzXy2gu9MWqJ5C6RNmxF\n" +
+        "c0+mENdG3OId4RnR9gBNJonc7EyPz6dEpB2SafXQ0/PwrxaPMesJDAABAKLiTquo\n" +
+        "xQdVoI/Zbviq2JLxbV2UlHeYEze3anX2LEMoESjN/wAAABQ8dXNlci0wQGV4YW1w\n" +
+        "bGUuY29tPsL/AAAAjgQQEwgAQP8AAAAFglartE3/AAAAAosJ/wAAAAmQ7u+h7OtC\n" +
+        "Cmb/AAAABZUICQoL/wAAAASWAwEC/wAAAAKbA/8AAAACngEAAIOBAP90TNLZ5e5D\n" +
+        "eAnZoIt5tN8bg+TPiYJKDWOglsZSR8jnWwEAo/e6JlLPHdSyQG3nQe/jmcGvPL1I\n" +
+        "AYUNC5NWm3BJUO/H/wAAAHsEAAAAABIIKoZIzj0DAQcCAwTpN0eqkzXDgrstHeSW\n" +
+        "6zKIFDHhzSRDR+mqZrReUJENOvsQLJzPelauT7umBP05fkONvoga4tGY1ZdFeIkz\n" +
+        "fsioAwEIBwAA/3VDQ0gISiheFRguXJn9S+0+wOQdYWkPdVaVBL0GKL/+DYPC/wAA\n" +
+        "AG0EGBMIAB//AAAABYJWq7RN/wAAAAmQ7u+h7OtCCmb/AAAAApsMAADVzAEAxNKU\n" +
+        "Gb3xRCsFlwGgwx07qMGrUF4Y7sh2OZASNX3sTf4BAJFguEEVhU9z+SkElXyphb+q\n" +
+        "PC6719E4g9mp1nK5myCrxv8AAABSBAAAAAATCCqGSM49AwEHAgMESZrm3yXlt+Kg\n" +
+        "UzXy2gu9MWqJ5C6RNmxFc0+mENdG3OId4RnR9gBNJonc7EyPz6dEpB2SafXQ0/Pw\n" +
+        "rxaPMesJDM3/AAAAFDx1c2VyLTBAZXhhbXBsZS5jb20+wv8AAACOBBATCABA/wAA\n" +
+        "AAWCVqu0Tf8AAAACiwn/AAAACZDu76Hs60IKZv8AAAAFlQgJCgv/AAAABJYDAQL/\n" +
+        "AAAAApsD/wAAAAKeAQAAg4EA/3RM0tnl7kN4Cdmgi3m03xuD5M+JgkoNY6CWxlJH\n" +
+        "yOdbAQCj97omUs8d1LJAbedB7+OZwa88vUgBhQ0Lk1abcElQ787/AAAAVgQAAAAA\n" +
+        "EggqhkjOPQMBBwIDBOk3R6qTNcOCuy0d5JbrMogUMeHNJENH6apmtF5QkQ06+xAs\n" +
+        "nM96Vq5Pu6YE/Tl+Q42+iBri0ZjVl0V4iTN+yKgDAQgHwv8AAABtBBgTCAAf/wAA\n" +
+        "AAWCVqu0Tf8AAAAJkO7voezrQgpm/wAAAAKbDAAA1cwBALFOYLzzlUL4mVEBywUJ\n" +
+        "QyI4ZVnWO5BUhDtuxSMByC5vAP9LKf7iBwfAa3R7mRIMD675PFqKdAFNiTDv5A5g\n" +
+        "JxPp1Q==\n" +
+        "=BvAW\n" +
+        "-----END PGP PRIVATE KEY BLOCK-----\n"
+ },
   {
-    "private": "-----BEGIN PGP PRIVATE KEY BLOCK-----\n" +
-        "Version: GnuPG v2\n" +
+    "private":
+        "-----BEGIN PGP PRIVATE KEY BLOCK-----\n" +
+        "Charset: UTF-8\n" +
+        "Version: End-To-End v0.31337.1\n" +
         "\n" +
-        "lKUEVqKhlBMIKoZIzj0DAQcCAwTxOl087JaSP14G1sHGacKOJkgaVnpOgc0PVQle\n" +
-        "kpv7bAWEEm4qy0l+LQuACFnf1f0WJLlsRpyoiJrHuseqT2Jn/gcDAsHo7L1c5nII\n" +
-        "4t0GZCtNhhzD+jFlKy5uJ5GBc3qeHJw8/lO57KBbftP8toqyhXlwdHSuaH5ykTDT\n" +
-        "mr1ZituwCdD9LMAhJy/7T+JPlDcCdYS0G3VzZXItMSA8dXNlci0xQGV4YW1wbGUu\n" +
-        "Y29tPoh5BBMTCAAhBQJWoqGUAhsDBQsJCAcCBhUICQoLAgQWAgMBAh4BAheAAAoJ\n" +
-        "ENVWGTIGMA5gU4wBALgTYRO/XCGF1zewnoBdXRU1OX3bAx3n7JV5Eor528TbAQCb\n" +
-        "qyLw2E6Ybpe6qfFpQWYoNS120eiuA1ajCQAyT8sbCpypBFaioZQSCCqGSM49AwEH\n" +
-        "AgMEYosxhZvy06PqgnfhmqCQ++VmdWqEc6b98HcfcdVXYiZBzL5ctmpPjzdKDKa9\n" +
-        "bPtVHEOaE0xsTjabf8YKiQ/75gMBCAf+BwMCTVAg716V2PjiezwFE45Z24HhqXMf\n" +
-        "wzoMWooWxihQbGNLlbjnrXGTLfm2RStcaa7rZ6J+VeSgQUs7Y1GCjU7gccMFAif6\n" +
-        "QRr93tLJ1q8rrohhBBgTCAAJBQJWoqGUAhsMAAoJENVWGTIGMA5gj6cBAJXHQVHH\n" +
-        "cYmpvO3Z5cZCMVJWKUOvPCv3o10QBOrcRH0RAP4gw+7nQqsZw+hBOovNmMgiJJM+\n" +
-        "Kc2gXqqYhoFBiSqZDQ==\n" +
-        "=WcnX\n" +
-        "-----END PGP PRIVATE KEY BLOCK-----",
-    "public": "-----BEGIN PGP PUBLIC KEY BLOCK-----\n" +
-        "Version: GnuPG v2\n" +
-        "\n" +
-        "mFIEVqKhlBMIKoZIzj0DAQcCAwTxOl087JaSP14G1sHGacKOJkgaVnpOgc0PVQle\n" +
-        "kpv7bAWEEm4qy0l+LQuACFnf1f0WJLlsRpyoiJrHuseqT2JntBt1c2VyLTEgPHVz\n" +
-        "ZXItMUBleGFtcGxlLmNvbT6IeQQTEwgAIQUCVqKhlAIbAwULCQgHAgYVCAkKCwIE\n" +
-        "FgIDAQIeAQIXgAAKCRDVVhkyBjAOYFOMAQC4E2ETv1whhdc3sJ6AXV0VNTl92wMd\n" +
-        "5+yVeRKK+dvE2wEAm6si8NhOmG6XuqnxaUFmKDUtdtHorgNWowkAMk/LGwq4VgRW\n" +
-        "oqGUEggqhkjOPQMBBwIDBGKLMYWb8tOj6oJ34ZqgkPvlZnVqhHOm/fB3H3HVV2Im\n" +
-        "Qcy+XLZqT483SgymvWz7VRxDmhNMbE42m3/GCokP++YDAQgHiGEEGBMIAAkFAlai\n" +
-        "oZQCGwwACgkQ1VYZMgYwDmCPpwEAlcdBUcdxiam87dnlxkIxUlYpQ688K/ejXRAE\n" +
-        "6txEfREA/iDD7udCqxnD6EE6i82YyCIkkz4pzaBeqpiGgUGJKpkN\n" +
-        "=eghU\n" +
-        "-----END PGP PUBLIC KEY BLOCK-----"
+        "xf8AAAB3BAAAAAATCCqGSM49AwEHAgMEFBwMuJYcFZ8diyWqImSpcjIDVXvrvTqi\n" +
+        "3ZW6aP2Xa7hM1RzpXNEJWf0MmSXcYPDF10t1NYGyFObi9MRGoVI1uAABAIpkPlZ0\n" +
+        "ekouOO8LrzBQyBbeR8/E+BrC5/SVweBFSHeqEHPN/wAAABQ8dXNlci0xQGV4YW1w\n" +
+        "bGUuY29tPsL/AAAAjgQQEwgAQP8AAAAFglartIL/AAAAAosJ/wAAAAmQXj/bCxIp\n" +
+        "OcD/AAAABZUICQoL/wAAAASWAwEC/wAAAAKbA/8AAAACngEAALZtAP40gWxAFmWi\n" +
+        "QtX0NcwueplZ9NGDXRmiZ/nib0YUDLISUgD/QlSj7uQMeakFNjXxRz8V1BqzyMRC\n" +
+        "kxzgWJz+n5S8KuPH/wAAAHsEAAAAABIIKoZIzj0DAQcCAwS1SHoSCjjOKdpzbUN/\n" +
+        "eIFRG9YdMaUUzFmDQ7UhbKZP/MNcBdACy87jVwI2tNh5rSWLhqPiEegMqTXZsqxd\n" +
+        "2ki3AwEIBwAA/1iPkMsIvyzP2idPN2y+wFY/zmDG2xR8nzVNEcXnZCR9EEvC/wAA\n" +
+        "AG0EGBMIAB//AAAABYJWq7SC/wAAAAmQXj/bCxIpOcD/AAAAApsMAADG6AD/RDyt\n" +
+        "sJ4dQ7FNACg5kvuC8y4MhO6EsoStVZSHgFXx6z8A/3y62Mh4rdBtjstmZase21OV\n" +
+        "Bzyi7hW5y4rRJHRSg1bJxv8AAABSBAAAAAATCCqGSM49AwEHAgMEFBwMuJYcFZ8d\n" +
+        "iyWqImSpcjIDVXvrvTqi3ZW6aP2Xa7hM1RzpXNEJWf0MmSXcYPDF10t1NYGyFObi\n" +
+        "9MRGoVI1uM3/AAAAFDx1c2VyLTFAZXhhbXBsZS5jb20+wv8AAACOBBATCABA/wAA\n" +
+        "AAWCVqu0gv8AAAACiwn/AAAACZBeP9sLEik5wP8AAAAFlQgJCgv/AAAABJYDAQL/\n" +
+        "AAAAApsD/wAAAAKeAQAAtm0A/jSBbEAWZaJC1fQ1zC56mVn00YNdGaJn+eJvRhQM\n" +
+        "shJSAP9CVKPu5Ax5qQU2NfFHPxXUGrPIxEKTHOBYnP6flLwq487/AAAAVgQAAAAA\n" +
+        "EggqhkjOPQMBBwIDBLVIehIKOM4p2nNtQ394gVEb1h0xpRTMWYNDtSFspk/8w1wF\n" +
+        "0ALLzuNXAja02HmtJYuGo+IR6AypNdmyrF3aSLcDAQgHwv8AAABtBBgTCAAf/wAA\n" +
+        "AAWCVqu0gv8AAAAJkF4/2wsSKTnA/wAAAAKbDAAAxugBAMXSmje/LKMk1EyIrn4+\n" +
+        "xcwKRzUiuDeg1EKsUWGcIVwcAPoCJlH/uaVStEdVDaLRqgvhYzUJOMWZgBz1Vn7I\n" +
+        "JmFLrg==\n" +
+        "=UvKo\n" +
+        "-----END PGP PRIVATE KEY BLOCK-----\n"
   }
 ];
 
@@ -146,43 +140,45 @@ function generateNonceAndHashes() {
 }
 
 // Load key(s)
-function SearchAlicesKey() {
-  console.log("SearchAlicesKey: running");
-  alice.searchPrivateKey("user-0@example.com").then(
+function SearchAlicesKey(param) {
+  console.log("SearchAlicesKey: running with param ", param);
+  alice.searchPrivateKey("<user-0@example.com>").addCallback(
       function(v) {
         console.log("searchPrivateKey: Success", v);
-      },
-      LogFailureAndExit);
+      })
+  .addErrback(function(err) {
+    console.log("searchaliceskey: failed:", err);
+  });
 }
 function LoadAlicesKey() {
   console.log("Loading Alice's Key");
   alice.importKeypair(loaded_messages.keys.password,
-                      "user-0@example.com",
-                      loaded_messages.keys.keys[0].private).then(
-                          SearchAlicesKey,
-                          SearchAlicesKey);
+                      "<user-0@example.com>",
+                      loaded_messages.keys.keys[0].private)
+                          .then(LoadBobsKey, LogFailureAndExit);
+
 }
 
 
 function LoadBobsKey() {
   console.log("Loading Bob's Key");
   bob.importKeypair(loaded_messages.keys.password,
-                    "user-1@example.com",
-                    loaded_messages.keys.keys[1].private).then(
-                        GenMessages,
-                        LogFailureAndExit
-                        );
+                    "<user-1@example.com>",
+                    loaded_messages.keys.keys[1].private).then(GenMessages, LogFailureAndExit);
 }
 
 // Generate Message(s)
 function GenMessages() {
   console.log("Generating messages.");
+  process.exit(0);
 }
 
 freedom.freedom('node_modules/freedom-pgp-e2e/dist/pgpapi.json', {}).then(
     function(proto) {
       alice = new proto();
       bob = new proto();
+//      alice.setup('', "user-0@example.com");
+//      bob.setup('', "user-1@example.com");
       // Define a JSON blob for the conversation.  We can only
       // generate messages that we have dependencies for.
       if (argv.init) {
